03ed625145d3e544678343c7d1b96599bccb33f6
musqat <hjs90561@naver.com>
2025-04-05 23:44:08 +0900

    first commit

---
+++ src/main/java/com/muscat/Collabus/CollabusApplication.java
@@ -0,0 +1,13 @@
+package com.muscat.Collabus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muscat/Collabus/CollabusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muscat.Collabus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
